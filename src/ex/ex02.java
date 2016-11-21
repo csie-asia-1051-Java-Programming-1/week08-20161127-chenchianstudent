3227707d6849e3914796ba737a4901bb4c7e6f7a
chenchianstudent <105021007@live.asia.edu.tw>
2016-11-21 18:34:06 +0800

    ex end

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -11,13 +11,17 @@ public class ex02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Scanner scn=new Scanner(System.in);
-System.out.print("請輸入");
+System.out.print("請輸入1-10");
 int n=scn.nextInt();
-	fun1(n);
-	}
+
+fun1(n);
+
+}
+
+	
 public static void fun1(int n){
 	Scanner scn=new Scanner(System.in);
-	System.out.print("請輸入要轉的數字系統(轉二位打2  轉16位打16)");
+	System.out.print("請輸入要轉的數字系統(轉二進位打2  轉16進位打16 轉8進位打8)");
 	int s=scn.nextInt();
 	if(s==2){int k,z;
 	String ans="";
@@ -37,6 +41,9 @@ public static void fun1(int n){
 	}else{
 	if(s==16){
 		String ans=Integer.toHexString(n);
+		System.out.print(ans);
+	}else{
+		String ans=Integer.toOctalString(n);
 		System.out.print(ans);
 	}	
 	}
--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -1,7 +1,11 @@
 ﻿package ex;
+import java.util.Scanner;
 /*
- * Topic:  寫一程式計算兩個矩陣的乘法，例如 A 是 n1 x m1 的矩陣， B 是 n2 x m2 的矩 陣，C=AB 為 n1 x m2 矩陣
-	第一列輸入 n1, m1, n2 及 m2 等 4 個整數，n1, m1 是第一個陣列的列(row)與行 (column)，而 n2, m2 則是第二個陣列的列(row)與行(column)，其中 m2 = n2。接著輸 入兩個陣列的內容。
+ * Topic:  寫一程式計算兩個矩陣的乘法，例如 A 是 n1 x m1 的矩陣， B 是 n2 x m2 的矩 陣，C=AB 為 n1 x m2矩陣
+	第一列輸入 n1, m1, n2 及 m2 等 4 個整數，n1, m1 是第一個陣列的列(row)與行 (column)，
+	而 n2, m2 則是第二個陣列的列(row)與行(column)，其中 m1= n2。接著輸 入兩個陣列的內容。
+	
+
  * Date: 2016/11/21
  * Author: 105021007 陳麒安
  */
@@ -10,7 +14,54 @@ public class ex03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
+Scanner scn=new Scanner(System.in);
+System.out.print("請輸入n1");
+int n1=scn.nextInt();
+System.out.print("請輸入m1");
+int m1=scn.nextInt();
+System.out.print("請輸入n2");
+int n2=scn.nextInt();
+System.out.print("請輸入m2");
+int m2=scn.nextInt();
+int[][] data=new int[n1][m1];
+int[][] data1=new int[n2][m2];
+int[][] data2=new int[data.length][data1[0].length];
+for(int i=0;i<data.length;i++){
+		for(int j=0;j<data[i].length;j++){
+			int num=scn.nextInt();
+			data[i][j]=num;	
+		}}
+		for(int i1=0;i1<data1.length;i1++){
+		for(int j1=0;j1<data1[i1].length;j1++){
+			int num=scn.nextInt();
+			data1[i1][j1]=num;		
+		}}
+		//以上為輸入數字
+		for(int i = 0; i<data.length;i++){
+			for(int j = 0; j<data[0].length;j++){
+				System.out.print(data[i][j]+"\t");
+			}
+			System.out.println();
+		}
+		for(int i = 0; i<data1.length;i++){
+			for(int j = 0; j<data1[0].length;j++){
+				System.out.print(data1[i][j]+"\t");
+			}
+			System.out.println();
+		}
+		//data data1 陣列
+		for(int i=0;i<data.length;i++){
+			for(int j=0;j<data1[0].length;j++){
+				for(int k=0;k<data[0].length;k++){
+				data2[i][j]+=data[i][k]*data1[k][j];
+			}
+			}
+		}	
+		for(int i = 0; i<data2.length;i++){
+			for(int j = 0; j<data2[0].length;j++){
+				System.out.print(data2[i][j]+"\t");
+			}
+			System.out.println();
+		}
+}
 }
